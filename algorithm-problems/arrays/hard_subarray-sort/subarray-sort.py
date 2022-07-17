2010bfb6c85a001b446ae247877899b56b36f0fd
Boyang Li <bryanli2009@live.ca>
2022-07-17 15:30:01 -0400

    Add 'subarray-sort'.

---
+++ algorithm-problems/arrays/hard_subarray-sort/subarray-sort.py
@@ -0,0 +1,29 @@
+# O(n) time | O(1) space - where n is the length of the input array
+def subarraySort(array):
+    minOutOfOrder = float("inf")
+    maxOutOfOrder = float("-inf")
+    for i in range(len(array)):
+        num = array[i]
+        if isOutOfOrder(i, num, array):
+            minOutOfOrder = min(minOutOfOrder, num)
+            maxOutOfOrder = max(maxOutOfOrder, num)
+
+    # handle the edge case that the array is perfectly sorted
+    if maxOutOfOrder == float("-inf"):
+        return [-1, -1]
+
+    subarrayLeftIdx = 0
+    while minOutOfOrder >= array[subarrayLeftIdx]:
+        subarrayLeftIdx += 1
+    subarrayrightIdx = len(array) - 1
+    while maxOutOfOrder <= array[subarrayrightIdx]:
+        subarrayrightIdx -= 1
+
+    return [subarrayLeftIdx, subarrayrightIdx]
+
+def isOutOfOrder(i, num, array):
+    if i == 0:
+        return num > array[i + 1]
+    if i == len(array) - 1:
+        return num < array[i - 1]
+    return num > array[i + 1] or num < array[i - 1]
