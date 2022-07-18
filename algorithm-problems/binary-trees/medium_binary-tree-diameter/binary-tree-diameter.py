bc804dcd13e9db172d339b0c143bf2a850d33e8b
Boyang Li <bryanli2009@live.ca>
2022-07-18 10:24:29 -0400

    Add 'binary tree diameter'.

---
+++ algorithm-problems/binary-trees/medium_binary-tree-diameter/binary-tree-diameter.py
@@ -0,0 +1,27 @@
+class BianryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class TreeInfo:
+    def __init__(self, diameter, height):
+        self.diameter = diameter
+        self.height = height
+
+def binaryTreeDiameter(tree):
+    return getTreeInfo(tree).diameter
+
+def getTreeInfo(tree):
+    if tree is None:
+        return TreeInfo(0, 0)
+
+    leftTreeInfo = getTreeInfo(tree.left)
+    rightTreeInfo = getTreeInfo(tree.right)
+
+    longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height
+    maxDiameterSoFar = max(leftTreeInfo.diameter, rightTreeInfo.diameter)
+    currDiameter = max(longestPathThroughRoot, maxDiameterSoFar)
+    currHeight = max(leftTreeInfo.height, rightTreeInfo.height) + 1
+
+    return TreeInfo(currDiameter, currHeight)
