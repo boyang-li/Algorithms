c831ff262ce60de3f6791aa6a5b25d829c379c67
Boyang Li <bryanli2009@live.ca>
2022-07-28 14:09:57 -0400

    Update notes for 'three number sum'.

--- algorithm-problems/arrays/medium_three-number-sum/three-number-sum.py
+++ algorithm-problems/arrays/medium_three-number-sum/three-number-sum.py
@@ -1,39 +1,49 @@
-# Brute-force approach: having triple nested loops, and try to
-# find every possible triplet that add up to the target sum.
-# Meanwhile, we still have to make sure no to include duplicates and
-# maintain the ascending order in the triplets.
+# Notes:
+# The brute-force approach uses a triple nested loop, we go through all possible
+# triplets, and find the ones that sum up to targetSum. We have to make sure
+# no duplicate is added, and the triplet elements are ascendingly ordered. This
+# approach would have a Time Complexity of O(nˆ3).
 #
-# Better approach: sort the array of integers in ASC order.
-# start by looping thourgh the sorted array, for each integer,
-# set up a left pointer and a right pointer at the indices i+1
-# and (len(array)-1) such that the three elements in a potential
-# matching triplet would be represented by the values at i, let and
-# right(natualy in ASC order due to the sorted array).
+# Better approach is to first sort the array, and construct a double nested
+# loop to go through each element of the array in order. In each iteration, we
+# find the rest of the array, and set up two pointers at the left and right
+# extremities, then uses a second loop to bring left and right to each other.
 #
-# Then within that iteration, we start a nested loop to increment
-# the left pointer or to decrement the right pointer based on
-# the sum of the three integers, and we continue as long as the left
-# and right do not pass each other. Whenever the three integers add up
-# to the targetSum, we add a tripplet to our output. By this setup,
-# we are guranteed to have the triplets in ASC order and free of
-# duplicates.
+# In the nested loop, we calculate the sum of Array[i], Array[left] and
+# Array[right], and add the triplet to the output whenever we finf a match, and
+# continue the loop until left passes right.
+#
+# We take the advantage of the fact that the array is sorted, and increment left
+# or decrement right to make the sum closer to targetSum. Naturally, the numbers
+# in the triplets will always be in ascending order, and free of duplicates.
+#
+# Complexity:
+# We have a double nested loop, and an auxiliary data structure of size n for
+# the outout.
 # O(n^2) time | O(n) space - where n is the length of the input array.
 def threeNumberSum(array, targetSum):
+    # assume we could use the built-in sorting function.
     array.sort()
     triplets = []
-    # we stop at the 3rd last number because we need to make
-    # space for the left and right pointers
+    # the loop stops at the 3rd element from the end, so that we won't go out of
+    # bound when we calculate a triplet.
     for i in range(len(array) - 2):
         left = i + 1
         right = len(array) - 1
+        # the loop stops when left completely passes right
         while left < right:
+            # sum of the triplet
             currSum = array[i] + array[left] + array[right]
+            # we found a match
             if currSum == targetSum:
                 triplets.append([array[i], array[left], array[right]])
                 left += 1
                 right -= 1
+            # under targetSum, increment left by 1 to make sum larger
             elif currSum < targetSum:
                 left += 1
+            # over targetSum, decrement right by 1 to make sum smaller
             elif currSum > targetSum:
                 right -= 1
+
     return triplets
