b58bec9beab2a4b20958b5ebbd3dec7b96425373
Boyang Li <bryanli2009@live.ca>
2022-07-28 13:35:16 -0400

    Update notes for 'two number sum'.

--- algorithm-problems/arrays/easy_two-number-sum/two-number-sum.py
+++ algorithm-problems/arrays/easy_two-number-sum/two-number-sum.py
@@ -1,15 +1,34 @@
+# Notes:
+# The brute-force approach to solve it would be having a double loop to go
+# through every pair of numbers, and check if they sum up to targetSum.
+#
+# The optimal solution uses a single loop to iterate through the array, and
+# uses a hash table to store the numbers we have seen.
+#
+# For each numbers, we calculat the difference between the number and the
+# targetSum, and we check whether the difference is in the hash table. If it is
+# in it, it means we have found a valid pair.
+#
+# Complexity:
+# The solution consists of a single loop of size n and an auxiliary data
+# structure of size n.
+#
 # O(n) time | O(n) space - where n is the length of the input array
 def twoNumberSum(array, targetSum):
+    # use a hash table to store the numbers seen
     seen = {}
+    # the result array
     twoSums = []
+
     for num in array:
-        # calculate the difference between target sum and current number
+        # get the difference between the number and target sum
         difference = targetSum - num
         # if we have seen the difference number before, it means we have found
-        # a pair, and we append it to the output array; Otherwise, we mark the
-        # number as seen by saving it as a key to the hash table with value True.
+        # a pair, and we append it to the output array; Otherwise, we update
+        # seen to True for the number in the hash table.
         if difference in seen:
             return [num, difference]
         else:
             seen[num] = True
+
     return twoSums
