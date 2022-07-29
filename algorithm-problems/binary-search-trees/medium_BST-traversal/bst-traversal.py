3a01eaaf7b3288c9a0d859454f8d7a74fbc8547a
Boyang Li <bryanli2009@live.ca>
2022-07-29 17:37:10 -0400

    Update the notes for 'BST traversal'.

--- algorithm-problems/binary-search-trees/medium_BST-traversal/bst-traversal.py
+++ algorithm-problems/binary-search-trees/medium_BST-traversal/bst-traversal.py
@@ -1,7 +1,21 @@
-# we traverse the BST recursively, append the node in 
-# appropriate order, and stop when the current node is None.
+# Notes:
+# Recall that the InOrder, PreOrder, and PostOrder are recursive traversal
+# functions defined as the following:
+# - InOrder: traverse the left child recursively, record the current node value,
+# and then traverse the right child recursively.
 #
-# O(n) time | O(n) space
+# - PreOrder: record the current node value, traverse the left child recursively
+# and then traverse the right child recursively.
+#
+# - PostOrder: traverse the left child recursively, traverse the right child
+# recursively, and then record the current node value.
+#
+# Stop the traversal when the current node is None.
+#
+# Complexity:
+# We are going to visit every node in the BT, and there could be at most n
+# recursive calls on the memory stack.
+# O(n) time | O(n) space - where n is the number of nodes in the BT.
 def inOrderTraverse(tree, array):
   if tree is not None:
     inOrderTraverse(tree.left, array)
