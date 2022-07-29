a1443bda006b40b71f45326d1dd0f3223bd56d3a
Boyang Li <bryanli2009@live.ca>
2022-07-28 23:36:13 -0400

    Update notes for 'merge overlapping intervals'.

--- algorithm-problems/arrays/medium_merge-overlapping-intervals/merge-overlapping-intervals.py
+++ algorithm-problems/arrays/medium_merge-overlapping-intervals/merge-overlapping-intervals.py
@@ -1,7 +1,14 @@
-# Optimal solution
-# note here we are assuming the input param 'intervals'contains
-# at least one element.
-#  
+# Notes:
+# For the optimal solution, we first sort the intervals by its starting index.
+# From the first interval, we start iterating all the intervals and check
+# whether the current interval is overlapping or is back-to-back to the next
+# interval.
+#
+# We merge all the overlapping or connected intervals and add them to the ourput.
+#
+# We are assuming the input param 'intervals'contains least one element.
+#
+# Complexity:
 # O(nlog(n)) time | O(n) space - where n is the number of intervals
 def mergeOverlappingIntervals(intervals):
   # Sorted the intervals by starting value
@@ -15,7 +22,11 @@ def mergeOverlappingIntervals(intervals):
     _, currIntervalEnd = currInterval
     nextIntervalStart, nextIntervalEnd = nextInterval
 
+    # compare the end index of the current interval with the start index of the
+    # next interval.
     if currIntervalEnd >= nextIntervalStart:
+      # we want to take the larger end index as the end index of the merged
+      # interval
       currInterval[1] = max(currIntervalEnd, nextIntervalEnd)
     else:
       currInterval = nextInterval
