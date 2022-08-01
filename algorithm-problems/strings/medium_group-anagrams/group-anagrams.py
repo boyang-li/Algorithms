b7606829abf677089a29f94ffb1f801394a22fb2
Boyang Li <bryanli2009@live.ca>
2022-08-01 08:55:02 -0400

    Update the notes for 'group anagrams'.

--- algorithm-problems/strings/medium_group-anagrams/group-anagrams.py
+++ algorithm-problems/strings/medium_group-anagrams/group-anagrams.py
@@ -1,5 +1,22 @@
+# Notes:
+# Anagrams are string made up of exactly the same letters, where order doesn't
+# matter. In this problem, we are given an array of words, and we need to return
+# a 2-d array which sort the words into groups of anagram for the same set of
+# letters.
+#
+# We first sort each words by the ASCII code for letters, so that the anagrams
+# for the same words become identical. Next, we use a hash table to store the
+# sorted words, and anagrams's index would be appended to the same key as groups.
+# Finaly, we iterate through the hash table values, and construct the 2-d array.
+#
+# Complexity:
+# Sorting would run in O(nlog(n)), and we run sorting for each of the input
+# words. Iterating the sorted words runs in O(n) time, and iterating the
+# hash table for anagram groups run in O(n) at its worst scenario. Both of them
+# can be canceled in big-O notaion.
+#
 # O(w * n * log(n)) time | O(wn) space - where w is the number of words and
-# n is the length of the longest word
+# n is the length of the longest word.
 def groupAnagrams(words):
     sortedWords = []
     for i in range(len(words)):
