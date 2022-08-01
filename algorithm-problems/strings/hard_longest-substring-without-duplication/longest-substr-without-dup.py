91673b08af47cecb66f0828aea5b68f8dac7fb58
Boyang Li <bryanli2009@live.ca>
2022-08-01 11:28:46 -0400

    Update the notes for 'longest substr without dup.'

--- algorithm-problems/strings/hard_longest-substring-without-duplication/longest-substr-without-dup.py
+++ algorithm-problems/strings/hard_longest-substring-without-duplication/longest-substr-without-dup.py
@@ -1,3 +1,10 @@
+# Notes:
+# We are solving this problem using a hash table which stores the letters we
+# have seen, and maintain a pointer indicating the start position of a substring
+# without duplicate in each iterations as we traverse the input string. We keep
+# track of the interval of the longest non-dup substring as we iterate.
+#
+# Complexity:
 # O(n) time | O(min(n, a)) - where n is the length of the string,
 # and a is the maximum number of unique characters in the substring.
 def longestSubstringWithoutDuplication(string):
@@ -13,10 +20,15 @@ def longestSubstringWithoutDuplication(string):
             # between startIdx and the one next to the duplicate.
             startIdx = max(startIdx, lastSeen[char] + 1)
 
+        # so long as the substring starting from the startIdx up until the
+        # current index, we extend the substring as i gets incremented and
+        # keep track of the longest non-dup substring as an interval of
+        # startIdx and i + 1(we use this for string slicing so it is exclusive).
         if longest[1] - longest[0] < i + 1 - startIdx:
-            # now if the current substring is greater than
-            # the longest on record, update longest
+            # now if the current substring is greater than the longest so far,
+            # update the interval.
             longest = [startIdx, i + 1]
+        # do not forget to update lastSeen hash table at the end.
         lastSeen[char] = i
 
     return string[longest[0]:longest[1]]
