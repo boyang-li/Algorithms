3b30c3e2907695d39bc32b89c0f3993cccc154c6
Boyang Li <bryanli2009@live.ca>
2022-08-01 22:17:34 -0400

    Update the notes for 'underscorify substring'.

--- algorithm-problems/strings/hard_underscorify-substring/underscorify-substring.py
+++ algorithm-problems/strings/hard_underscorify-substring/underscorify-substring.py
@@ -1,45 +1,51 @@
-# we break the algorithm into three functions:
-# getLocations(), collapse(), and underscorify()
-# this way, the code is much more easier to write and readable.
+# Notes:
+# This problem wants us to put underscores around target substrings in place,
+# and if multiple target substrings that are 'back-to-back', or overlapping,
+# we just put an underscore at each end of the chunk.
 #
+# A good approach which makes the code much more easier to implement, is to
+# break the algorithm down to three sub-operations: 'getLocations', 'collapse',
+# and 'underscorify'.
+#
+# Complexity:
 # Average case: O(n + m) | O(n) space - where n is the length
-# of the main string and m is the length of the substring
+# of the main string and m is the length of the substring.
 def underscorifySubstring(string, substring):
     locations = collapse(getLocations(string, substring))
     return underscorify(string, locations)
 
-# given the input string and the substring, find all the
-# index pairs of the substring instances in the input string.
-# i.e. locations = [[startIdx, endIdx], ...]
+# This function uses pointer to find matches of the target substring in the main
+# string, and returns the indices of the start and end index pairs of the
+# matches. i.e. locations = [[startIdx, endIdx], ...]
 def getLocations(string, substring):
     locations = []
+    # having a pointer to mark the beginning of the matching substring.
     startIdx = 0
     while startIdx < len(string):
-        # get the starting index of the first occurrence of the
-        # substring, find() returns -1 if not found
+        # get the starting index of the first occurrence of the target substring
+        # from the startIdx in the main string, find() returns -1 if not found.
         nextIdx = string.find(substring, startIdx)
         if nextIdx != -1:
+            # found a match, append the index interval of the substring.
             locations.append([nextIdx, nextIdx + len(substring)])
-            # note that next start index is 1 index further,
-            # because substrings can overlap each other
+            # note that next startIdx is the second index of the current
+            # matching substring, because substrings can overlap.
             startIdx = nextIdx + 1
         else: break
     return locations
 
-# collapse detects if any pair of the substring intervals in the
-# locations array are back-to-back or overlapping each other,
-# merge them if found.
+# This function traverse the intervals of the matching substring, and detects
+# if any intervals are back-to-back or overlapping, merge them if found.
 def collapse(locations):
     if not len(locations):
         return locations
-    # create a new array for output
+    # create a new array for output with the first element in place.
     newLocations = [locations[0]]
-    # compare the two adjacent elements at a time
+    # compare every two adjacent elements at a time
     previous = newLocations[0]
     for i in range(1, len(locations)):
         current = locations[i]
-        # check if the two locations are back-to-back or
-        # overlapping each other.
+        # check whether the intervals are back-to-back or overlapping.
         if current[0] <= previous[1]:
             # collapse a pair of locations by setting the prev's
             # end index to the current's end index.
@@ -47,33 +53,49 @@ def collapse(locations):
             # merely modifying its value.
             previous[1] = current[1]
         else:
-            # the locations are apart, and we just add the current
-            # location to the output array.
+            # the intervals are separate, add the current interval to the output
             newLocations.append(current)
+            # update prev so the loop continues
             previous = current
     return newLocations
 
-# given the collapsed locations array, we can now safely wrap
-# each of the elements with underscores.
+# This function actually put underscores around the target matches in place, and
+# it is called after the 'collape' operation.
 def underscorify(string, locations):
     locationsIdx = 0
     stringIdx = 0
-    # we are in a position where we have added the opening
-    # undercore, but still have not added the closing one.
+    # use a flag to indicate that we are in the position where we have already
+    # put the opening undercore, but have not close it yet.
     inBetweenUnderscores = False
+    # array of letters for the output string.
     finalChars = []
     i = 0
+    # traverse the main string and the locations intervals at the same time,
+    # and do stuff.
     while stringIdx < len(string) and locationsIdx < len(locations):
+        # whenever we hit an index matching any interval extremities, we have
+        # arrived a position to put an underscore.
         if stringIdx == locations[locationsIdx][i]:
             finalChars.append("_")
+            # flip the flag for open/closed underscores.
             inBetweenUnderscores = not inBetweenUnderscores
             if not inBetweenUnderscores:
+                # if we have closed an open underscore, it means we can go to
+                # the next interval.
                 locationsIdx += 1
+            # i is the index for intervals, which could either be start(0) or
+            # end(1) index.
             i = 0 if i == 1 else 1
+        # add the character to the output
         finalChars.append(string[stringIdx])
+        # increment the main string index
         stringIdx += 1
+    # this is to address the situation when the last character of the main string
+    # is part of the target substring, the closing underscore must be added.
     if locationsIdx < len(locations):
         finalChars.append("_")
+    # If we have not finish traversing the main string, we need to add the rest
+    # of the characters to the output.
     elif stringIdx < len(string):
         finalChars.append(string[stringIdx:])
     return "".join(finalChars)
