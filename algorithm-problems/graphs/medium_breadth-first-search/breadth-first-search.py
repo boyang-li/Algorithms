fca68b1e9e4138d90bc9a1ae4cf3fc90263b6752
Boyang Li <bryanli2009@live.ca>
2022-08-02 23:05:42 -0400

    Update the notes for 'breadth first search'.

--- algorithm-problems/graphs/medium_breadth-first-search/breadth-first-search.py
+++ algorithm-problems/graphs/medium_breadth-first-search/breadth-first-search.py
@@ -12,14 +12,20 @@ class Node:
     # (using a queue/FIFO data structure) before we go deeper.
     #
     # Complexity:
+    # Don't forget counting the edges of the graph when analyzing the time
+    # comlexity of a graph traversing problem.
     # O(v + e) time | O(v) space
     # v = number of vertices; e = number of edges
     def bfs(self, array):
         # If we were permitted to use a list as if it was a queue
         queue = [self]
+        # so long as the queue is not empty, continue the loop.
         while len(queue) > 0:
+            # pop the first node off the queue.
             curr = queue.pop(0)
+            # alway visit the node first
             array.append(curr.name)
+            # push all of the child nodes of the current node to the queue.
             for child in curr.children:
                 queue.append(child)
 
